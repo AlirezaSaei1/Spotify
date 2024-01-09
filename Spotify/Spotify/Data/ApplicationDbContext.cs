01fe0e7cdfcb2221ecb8a47858189aba15ded466
Alireza Saei <saeialireza359@gmail.com>
2024-01-09 21:24:59 +0330

    Created Project with custom user dereived from IdentityUser

--- Spotify/Spotify/Data/ApplicationDbContext.cs
+++ Spotify/Spotify/Data/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Spotify.Models;
 
 namespace Spotify.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
     }
 }
--- Spotify/Spotify/Models/ApplicationUser.cs
+++ Spotify/Spotify/Models/ApplicationUser.cs
@@ -4,5 +4,11 @@ namespace Spotify.Models;
 
 public class ApplicationUser : IdentityUser
 {
-    public string Name { get; set; }
+    public string Name { set; get; }
+    public DateTime AccountCreationTime { get; set; }
+    
+    public ApplicationUser()
+    {
+        AccountCreationTime = DateTime.Now;
+    } 
 }
